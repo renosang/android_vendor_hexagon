2d6aadf43e0b7b28dc1cc20f95914782034b23f1
Joe <joe2k01dev@gmail.com>
2017-07-20 19:27:01 +0200

    vendorsetup: Enable also other build variants

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,6 @@
 for device in $(curl -s https://raw.githubusercontent.com/HexagonRom/official_devices/master/devices.json | sed 's/ //; /^$/d' | grep -Po '\"codename\": ".*?"' | sed -e 's/codename//;s/\"//g;s/\: //')
 do
-add_lunch_combo hexagon_$device-userdebug
+for var in eng user userdebug; do
+add_lunch_combo hexagon_$device-$var
+done
 done
