24e971597f2a0639f64da25af264c6c3611fcffa
jhenrique09 <jhenrique09.mcz@hotmail.com>
2018-01-04 20:02:07 -0200

    vendor: Use python script to fetch official devices

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,4 @@
-for device in $(curl -s https://raw.githubusercontent.com/HexagonRom/official_devices/master/devices.json | sed 's/ //; /^$/d' | grep -Po '\"codename\": ".*?"' | sed -e 's/codename//;s/\"//g;s/\: //')
+for device in $(python vendor/hexagon/tools/get_official_devices.py)
 do
 for var in eng user userdebug; do
 add_lunch_combo hexagon_$device-$var
