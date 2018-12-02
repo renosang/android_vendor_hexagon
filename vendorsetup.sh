AospExtended/platform_vendor_aosp

Allow lunch menu to include all our official devices from official_devices/devices.json file
Signed-off-by: Shubham Singh <coolsks94@gmail.com>

vendorsetup.sh
@@ -1,0 +1,4 @@
+for device in $(curl -s https://raw.githubusercontent.com/AospExtended/official_devices/master/devices.json | sed 's/ //; /^$/d' | grep -Po '\"codename\": ".*?"' | sed -e 's/codename//;s/\"//g;s/\: //')
+do
+add_lunch_combo aosp_$device-userdebug
+done

renosang/android_vendor_hexagon

vendorsetup.sh
@@ -1,0 +1,4 @@
+for device in $(curl -s https://raw.githubusercontent.com/HexagonRom/official_devices/master/devices.json | sed 's/ //; /^$/d' | grep -Po '\"codename\": ".*?"' | sed -e 's/codename//;s/\"//g;s/\: //')
+do
+add_lunch_combo hexagon_$device-userdebug
+done